dde0ed9473be3ccc1ea929cdbd8210a97f90b593
ignaciomtp <ignaciomtp@gmail.com>
2026-04-21 09:48:01 +0200

    Modificación mensaje múltiples clientes

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    //
+    return $this->belongsToMany(Mensaje::class);
 }
--- app/Models/Mensaje.php
+++ app/Models/Mensaje.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mensaje extends Model
 {
-    //
+    return $this->belongsToMany(Cliente::class);
 }
